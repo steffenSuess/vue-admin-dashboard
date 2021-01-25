1297e52a01425fe883875543dc4244e5ad4aed89
Steffen Suess <kontakt@steffensuess.de>
2021-01-25 11:21:26 +0100

    -implemented SignIn-View

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue"
+import SignIn from "../views/SignInFlow/SignIn"
+import Request from "../views/SignInFlow/Request"
+import Recover from "../views/SignInFlow/Recover"
 
 Vue.use(VueRouter);
 
@@ -12,12 +16,23 @@ const routes = [
   },
   {
     path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    name: "about",
+    component: About
+  },
+  {
+    path: "/signin",
+    name: "signin",
+    component: SignIn
+  },
+  {
+    path: "/request",
+    name: "request",
+    component: Request
+  },
+  {
+    path: "/recover",
+    name: "recover",
+    component: Recover
   }
 ];
 
